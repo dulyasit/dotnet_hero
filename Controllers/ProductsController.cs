2b1980a124ff2e12c698777f200fda1542a66039
pdulyasit <pdulyasit@compupower-home.com>
2022-09-14 20:02:23 +0700

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,90 @@
+﻿using dotnet_hero.Data;
+using dotnet_hero.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Net;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+
+namespace dotnet_hero.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly DatabaseContext databaseContext;
+
+        public ProductsController(DatabaseContext databaseContext) => this.databaseContext = databaseContext;
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            return databaseContext.Products.OrderByDescending(p => p.ProductId).ToList();
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProductById(int id)
+        {
+            var result = databaseContext.Products.Find(id);
+            if (result == null)
+                return NotFound();
+            return result;
+        }
+        [HttpGet("search")]
+        public ActionResult<IEnumerable<Product>> SearchProducts([FromQuery] string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                name = "";
+            var result = databaseContext.Products
+                .Where(p => p.Name.ToLower().Contains(name.ToLower()))
+                .ToList();
+            //if (result == null)
+            //    return NotFound();
+            return result;
+        }
+        [HttpPost]
+        public ActionResult<Product> AddProduct([FromForm] Product model)
+        {
+            databaseContext.Products.Add(model);
+            databaseContext.SaveChanges();
+            return StatusCode((int)HttpStatusCode.Created);
+        }
+        [HttpPut("{id}")]
+        public ActionResult<Product> UpdateProduct(int id, [FromForm] Product model)
+        {
+            if (id != model.ProductId)
+            {
+                return BadRequest();
+            }
+            Product result = databaseContext.Products.Find(id);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+            result.Name = model.Name;
+            result.Price = model.Price;
+            result.Stock = model.Stock;
+            result.CategoryId = model.CategoryId; 
+
+            databaseContext.Products.Update(result);
+            databaseContext.SaveChanges();
+
+            return StatusCode((int)HttpStatusCode.NoContent);
+        }
+        [HttpDelete("{id}")]
+        public ActionResult DeleteProduct(int id)
+        {
+            Product result = databaseContext.Products.Find(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            databaseContext.Remove(result);
+            databaseContext.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using dotnet_hero.Entities;
+
+namespace dotnet_hero.Data
+{
+    public partial class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+        {
+        }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Account> Accounts { get; set; } = null!;
+        public virtual DbSet<Category> Categories { get; set; } = null!;
+        public virtual DbSet<Product> Products { get; set; } = null!;
+        public virtual DbSet<Role> Roles { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>(entity =>
+            {
+                entity.Property(e => e.AccountId).HasColumnName("AccountID");
+
+                entity.Property(e => e.Created).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.RoleId).HasColumnName("RoleID");
+
+                entity.HasOne(d => d.Role)
+                    .WithMany(p => p.Accounts)
+                    .HasForeignKey(d => d.RoleId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Accounts_Roles");
+            });
+
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+
+                entity.Property(e => e.Created).HasDefaultValueSql("(getdate())");
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+
+                entity.Property(e => e.Created).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Image).HasMaxLength(50);
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 0)");
+
+                entity.HasOne(d => d.Category)
+                    .WithMany(p => p.Products)
+                    .HasForeignKey(d => d.CategoryId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Products_Categories");
+            });
+
+            modelBuilder.Entity<Role>(entity =>
+            {
+                entity.Property(e => e.RoleId).HasColumnName("RoleID");
+
+                entity.Property(e => e.Created).HasDefaultValueSql("(getdate())");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
