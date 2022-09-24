dcf9730cc0b564c590420ed6d3ae024d2da74df3
pdulyasit <pdulyasit@compupower-home.com>
2022-09-24 20:55:57 +0700

    Register

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using dotnet_hero.DTOs.Account;
+using dotnet_hero.Entities;
+using dotnet_hero.Interfaces;
+using Mapster;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace dotnet_hero.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            this.accountService = accountService;
+        }
+        [HttpPost("[action]")]
+        public async Task<ActionResult> Register(RegisterRequest registerRequest)
+        {
+            var accout = registerRequest.Adapt<Account>();
+            await accountService.Register(accout);
+            return StatusCode((int)HttpStatusCode.Created);
+        }
+
+    }
+}
---
+++ DTOs/Account/RegisterRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dotnet_hero.DTOs.Account
+{
+    public class RegisterRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Username { get; set; }
+        [Required]
+        [MinLength(8)]
+        public string Password { get; set; }
+        public int RoleId { get; set; }
+    }
+}
